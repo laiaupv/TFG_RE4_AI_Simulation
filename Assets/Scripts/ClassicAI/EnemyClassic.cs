31cdccc442ed361c997e36fae52e0433dc2a2da9
laiaupv <laiaupvuwu@gmail.com>
2026-05-18 10:20:16 +0200

    feat(fsm-classic): radio de detección visual binario con LineRenderer

--- Assets/Scripts/ClassicAI/EnemyClassic.cs
+++ Assets/Scripts/ClassicAI/EnemyClassic.cs
@@ -10,6 +10,8 @@ public class EnemyClassic : MonoBehaviour
 
     private StateMachine _stateMachine;
     private Transform _player;
+    private LineRenderer _lineRenderer;
+    private int _circleSegments = 40;
 
     void Start()
     {
@@ -17,21 +19,56 @@ public class EnemyClassic : MonoBehaviour
         _stateMachine = new StateMachine();
         _stateMachine.ChangeState(new IdleState(
             transform, waypoints, _stateMachine, _player));
+
+        _lineRenderer = GetComponent<LineRenderer>();
+        DrawDetectionCircle();
     }
 
     void Update()
     {
         _stateMachine.Update();
+        UpdateCircleColor();
     }
 
-    // Dibuja el radio de detección en el editor
+    void DrawDetectionCircle()
+    {
+        _lineRenderer.positionCount = _circleSegments + 1;
+
+        for (int i = 0; i <= _circleSegments; i++)
+        {
+            float angle = i * 2 * Mathf.PI / _circleSegments;
+            float x = Mathf.Cos(angle) * detectionRadius;
+            float z = Mathf.Sin(angle) * detectionRadius;
+            _lineRenderer.SetPosition(i, new Vector3(x, 0.1f, z));
+        }
+    }
+
+    void UpdateCircleColor()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, 
+                                                   _player.position);
+        if (distanceToPlayer < detectionRadius)
+        {
+            // Rojo cuando detecta al jugador
+            _lineRenderer.startColor = Color.red;
+            _lineRenderer.endColor = Color.red;
+        }
+        else
+        {
+            // Amarillo cuando no detecta
+            _lineRenderer.startColor = Color.yellow;
+            _lineRenderer.endColor = Color.yellow;
+        }
+    }
+
     void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, detectionRadius);
     }
+
     public string GetCurrentState()
-{
-    return _stateMachine.GetCurrentStateName();
-}
+    {
+        return _stateMachine.GetCurrentStateName();
+    }
 }
